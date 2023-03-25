be8e083a026b64cf3be413c0065f4c3b038bbde6
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-03-25 08:53:24 +0100

    Added time passed on the progress bar

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -84,6 +86,23 @@ const updateProgressBar = (e) => {
         
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
 
